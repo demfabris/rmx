cea59318610f6b69a9146daf649362454f7e1ee3
Fabricio Dematte <demfabris@gmail.com>
2022-09-18 11:40:35 -0300

    add metadata to cargo.toml

--- src/interact.rs
+++ src/interact.rs
@@ -5,7 +5,7 @@ use crate::Result;
 /// # Errors
 ///
 /// Fails with I/O error if can't write to stdout
-#[cfg(not(feature = "no-interactive"))]
+#[cfg(not(feature = "auto-interactive"))]
 pub fn with_message(message: String) -> Result<bool> {
     Confirm::with_theme(&theme::ColorfulTheme::default())
         .with_prompt(message)
@@ -15,7 +15,7 @@ pub fn with_message(message: String) -> Result<bool> {
         .map_err(std::convert::Into::into)
 }
 
-#[cfg(feature = "no-interactive")]
+#[cfg(feature = "auto-interactive")]
 #[allow(clippy::needless_pass_by_value)]
 pub fn with_message(message: String) -> Result<bool> {
     println!("{}", message);
